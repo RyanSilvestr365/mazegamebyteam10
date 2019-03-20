4269e5cc06ac7c7e34269bf317324eee8486bd56
RyanSilvestr365 <48536655+RyanSilvestr365@users.noreply.github.com>
2019-03-20 16:32:36 -0700

    final update before play test

--- Maze game - assignment 2/Assets/Scripts/SettingsMenu.cs
+++ Maze game - assignment 2/Assets/Scripts/SettingsMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine.Audio;
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    
     public void SetVolume(float volume) {
         audioMixer.SetFloat("Volume", volume);
     }
